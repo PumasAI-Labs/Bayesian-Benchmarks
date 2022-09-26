9915ec031f6ec2c96a621a67b2ed3d7a3343cdda
Jose Storopoli <jose@storopoli.io>
2022-09-26 16:02:59 -0300

    initial draft

---
+++ Torsten-Runs/iv_2cmt_macro_ir1.jl
@@ -0,0 +1,32 @@
+ENV["CMDSTAN"] = joinpath(@__DIR__, "..", "Torsten", "cmdstan")
+using Stan
+using StanSample
+using DataFramesMeta
+
+# setting up CMDSTAN path to Torsten
+Stan.set_cmdstan_home!(joinpath(@__DIR__, "..", "Torsten", "cmdstan"))
+
+m_str = read(joinpath(@__DIR__, "..", "Torsten", "example-models", "iv_2cmt_macro_ir1", "iv_2cmt_macro_ir1_cl_vc_q_vp_kin_kout_ic50_ppa_lognormal_bloq_lower_coupled.stan"), String)
+m = SampleModel("iv_2cmt_macro_ir1", m_str)
+
+# data and inits
+include(joinpath(@__DIR__, "iv_2cmt_macro_ir1_data.jl"))
+
+rc = stan_sample(
+    m;
+    use_cpp_chains=true,
+    data=stan_data,
+    init=stan_init,
+    num_chains=4,
+    num_samples=1_000,
+    num_warmups=1_000,
+    delta=0.8
+)
+
+if success(rc)
+    summary_df = read_summary(m, false)
+end
+
+parameters_to_summarize = [:TVCL, :TVVC, :TVQ, :TVVP, :TVKA]
+
+@rsubset summary_df :parameters ∈ parameters_to_summarize
---
+++ Torsten-Runs/iv_2cmt_macro_ir1_data.jl
@@ -0,0 +1,20 @@
+using Distributions
+using JSON3
+
+json_string = read(joinpath(@__DIR__, "data", "iv_2cmt_macro_ir1_cl_vc_q_vp_kin_kout_ic50_ppa_lognormal_001_bloq.json"), String)
+
+stan_data = JSON3.read(json_string, NamedTuple)
+
+stan_init = (;
+    TVCL=rand(LogNormal(log(0.3), 0.3)),
+    TVVC=rand(LogNormal(log(5), 0.3)),
+    TVQ=rand(LogNormal(log(1), 0.3)),
+    TVVP=rand(LogNormal(log(10), 0.3)),
+    omega=rand(LogNormal(log(0.3), 0.3), 4),
+    sigma=rand(LogNormal(log(0.5), 0.3), 2),
+    TKKIN=rand(LogNormal(log(110), 0.3)),
+    TKKOUT=rand(LogNormal(log(1), 0.3)),
+    TVIC50=rand(LogNormal(log(6), 0.3)),
+    omega_pd=rand(LogNormal(log(0.3), 0.3), 3),
+    sigma_pd=rand(LogNormal(log(0.1), 0.3))
+)
