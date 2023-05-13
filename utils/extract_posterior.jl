fc96cc61b667df067cc7564ce2e5f1f94d7bda6f
José Storopoli <jose@pumas.ai>
2023-05-13 09:14:57 +0000

    added results

--- utils/extract_posterior.jl
+++ utils/extract_posterior.jl
@@ -4,6 +4,8 @@ using DataFrames
 using DataFramesMeta
 using CSV
 using Serialization
+using CairoMakie
+using AlgebraOfGraphics
 
 function get_chains_stan(
     arrow;
@@ -62,9 +64,16 @@ end
 
 function mean_ess_sec(chn)
     summ_df = DataFrame(summarystats(chn))
-    filter!(r -> !(isnan(r.ess_per_sec)), summ_df)
+    # filter!(r -> !(isnan(r.ess_per_sec)), summ_df)
     mean_ess_sec = mean(summ_df[:, :ess_per_sec])
     return mean_ess_sec
+end
+
+function mean_ess(chn)
+    summ_df = DataFrame(summarystats(chn))
+    # filter!(r -> !(isnan(r.ess_per_sec)), summ_df)
+    mean_ess = mean(summ_df[:, :ess])
+    return mean_ess
 end
 
 files_01 = filter(f -> endswith(f, ".arrow"), readdir(joinpath(pwd(), "01-iv_2cmt_linear", "Stan", "Torsten", "Fits"); join=true))
@@ -77,15 +86,31 @@ chn_03_mult_dose, chn_03_mult_dose_mat_exp, chn_03_single_dose, chn_03_single_do
 
 stan_df = DataFrame(;
     model=[
-    "01_mult_dose","01_mult_dose_mat_exp","01_single_dose","01_single_dose_mat_exp",
-    "02_mult_dose","02_mult_dose_mat_exp","02_single_dose","02_single_dose_mat_exp",
-    "03_mult_dose","03_mult_dose_mat_exp","03_single_dose","03_single_dose_mat_exp",
+    # "01_mult_dose","01_mult_dose_mat_exp","01_single_dose","01_single_dose_mat_exp",
+    # "02_mult_dose","02_mult_dose_mat_exp","02_single_dose","02_single_dose_mat_exp",
+    # "03_mult_dose","03_mult_dose_mat_exp","03_single_dose","03_single_dose_mat_exp",
+    "01_mult_dose","01_single_dose",
+    "02_mult_dose","02_single_dose",
+    "03_mult_dose","03_single_dose",
     ],
+    mean_ess=mean_ess.(
+        [
+            # chn_01_mult_dose, chn_01_mult_dose_mat_exp, chn_01_single_dose, chn_01_single_dose_mat_exp,
+            # chn_02_mult_dose, chn_02_mult_dose_mat_exp, chn_02_single_dose, chn_02_single_dose_mat_exp,
+            # chn_03_mult_dose, chn_03_mult_dose_mat_exp, chn_03_single_dose, chn_03_single_dose_mat_exp,
+            chn_01_mult_dose_mat_exp, chn_01_single_dose_mat_exp,
+            chn_02_mult_dose_mat_exp, chn_02_single_dose_mat_exp,
+            chn_03_mult_dose_mat_exp, chn_03_single_dose_mat_exp,
+        ]
+    ),
     mean_ess_sec=mean_ess_sec.(
         [
-            chn_01_mult_dose, chn_01_mult_dose_mat_exp, chn_01_single_dose, chn_01_single_dose_mat_exp,
-            chn_02_mult_dose, chn_02_mult_dose_mat_exp, chn_02_single_dose, chn_02_single_dose_mat_exp,
-            chn_03_mult_dose, chn_03_mult_dose_mat_exp, chn_03_single_dose, chn_03_single_dose_mat_exp,
+            # chn_01_mult_dose, chn_01_mult_dose_mat_exp, chn_01_single_dose, chn_01_single_dose_mat_exp,
+            # chn_02_mult_dose, chn_02_mult_dose_mat_exp, chn_02_single_dose, chn_02_single_dose_mat_exp,
+            # chn_03_mult_dose, chn_03_mult_dose_mat_exp, chn_03_single_dose, chn_03_single_dose_mat_exp,
+            chn_01_mult_dose_mat_exp, chn_01_single_dose_mat_exp,
+            chn_02_mult_dose_mat_exp, chn_02_single_dose_mat_exp,
+            chn_03_mult_dose_mat_exp, chn_03_single_dose_mat_exp,
         ]
     )
 )
@@ -101,10 +126,15 @@ nonmem_03_md = get_chains_nonmen(joinpath(pwd(), "03-depot_2cmt_linear", "NONMEM
 
 nonmem_df = DataFrame(;
     model=[
-    "01_single_dose","01_mult_dose_",
+    "01_single_dose","01_mult_dose",
     "02_single_dose","02_mult_dose",
     "03_single_dose","03_mult_dose",
     ],
+    mean_ess=mean_ess.(
+        [
+            nonmem_01_sd, nonmem_01_md, nonmem_02_sd, nonmem_02_md, nonmem_03_sd, nonmem_03_md
+        ]
+    ),
     mean_ess_sec=mean_ess_sec.(
         [
             nonmem_01_sd, nonmem_01_md, nonmem_02_sd, nonmem_02_md, nonmem_03_sd, nonmem_03_md
@@ -123,10 +153,15 @@ pumas_03_md = deserialize(joinpath(pwd(), "03-depot_2cmt_linear", "Pumas", "fit_
 
 pumas_df = DataFrame(;
     model=[
-    "01_single_dose","01_mult_dose_",
+    "01_single_dose","01_mult_dose",
     "02_single_dose","02_mult_dose",
     "03_single_dose","03_mult_dose",
     ],
+    mean_ess=mean_ess.(
+        [
+            pumas_01_sd, pumas_01_md, pumas_02_sd, pumas_02_md, pumas_03_sd, pumas_03_md
+        ]
+    ),
     mean_ess_sec=mean_ess_sec.(
         [
             pumas_01_sd, pumas_01_md, pumas_02_sd, pumas_02_md, pumas_03_sd, pumas_03_md
@@ -144,3 +179,42 @@ all_df = vcat(stan_df, nonmem_df, pumas_df)
 select!(all_df, :software, All())
 
 CSV.write("results/all.csv", all_df)
+
+summ_df = @chain all_df begin
+    groupby(:software)
+    @combine begin
+        :mean_ess = mean(:mean_ess)
+        :mean_ess_sec = mean(:mean_ess_sec)
+    end
+end
+
+CSV.write("results/summary.csv", summ_df)
+
+# Plots
+plt = data(all_df) *
+    mapping(
+        :model => renamer([
+            "01_single_dose" => "1 SD",
+            "01_mult_dose" => "1 MD",
+            "02_single_dose" => "2 SD",
+            "02_mult_dose" => "2 MD",
+            "03_single_dose" => "3 SD",
+            "03_mult_dose" => "3 MD",
+        ]),
+        [:mean_ess, :mean_ess_sec];
+        color=:software,
+        dodge=:software,
+        col=dims(1) => renamer(["Mean ESS", "Mean ESS/sec"]),
+    ) *
+    visual(BarPlot)
+
+fig = draw(
+    plt;
+    axis=(;
+        xticklabelrotation=π/3,
+        ylabel="",
+    ),
+    facet=(; linkyaxes=:none)
+)
+
+save("results/results.png", fig; px_per_unit=3)
