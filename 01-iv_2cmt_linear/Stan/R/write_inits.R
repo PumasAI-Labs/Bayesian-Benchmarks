44daa4a4f4d55b7a9e7dd5a628bfa9044f666965
Casey Davis <cbdavis33@gmail.com>
2023-06-08 11:11:30 -0700

    Write inits to json files (#70)
    * Write inits to json files
    
    * Change init in model$sample()

---
+++ 01-iv_2cmt_linear/Stan/R/write_inits.R
@@ -0,0 +1,32 @@
+rm(list = ls())
+cat("\014")
+
+library(tidyjson)
+library(tidyverse)
+
+n_subjects <- read_csv("01-iv_2cmt_linear/data/single_dose.csv",
+# nonmem_data <- read_csv("01-iv_2cmt_linear/data/multiple_dose.csv",
+                        na = ".") %>% 
+  distinct(ID) %>%
+  count() %>%
+  deframe()
+
+
+write_inits <- function(run, chain){
+  list(TVCL = rlnorm(1, log(4), 0.3),
+       TVVC = rlnorm(1, log(70), 0.3),
+       TVQ = rlnorm(1, log(4), 0.3),
+       TVVP = rlnorm(1, log(50), 0.3),
+       omega = rlnorm(4, log(0.3), 0.3),
+       sigma_p = rlnorm(1, log(0.2), 0.3),
+       L = diag(4),
+       Z = matrix(rnorm(n_subjects*4), nrow = n_subjects, ncol = 4)) %>% 
+    map(round, 3) %>% 
+    write_stan_json(str_c("01-iv_2cmt_linear/data/inits/inits_", 
+                          run, "_", chain, ".json"))
+}
+
+expand_grid(run = 1:10, chain = 1:4) %>% 
+  {map2(.$run, .$chain, write_inits)}
+
+
