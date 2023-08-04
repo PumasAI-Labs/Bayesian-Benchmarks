af93523d5ef2e358f37003d1730f27d7831c8738
cbdavis33 <cbdavis33@gmail.com>
2023-08-04 07:39:10 -0700

    utils

--- utils/run_chains.R
+++ utils/run_chains.R
@@ -2,7 +2,8 @@ library(furrr)
 library(future)
 plan(multisession)
 
-run_chains <- function(folder, nonmem_model, nchains=4, threads_per_chain=8) {
+run_chains <- function(folder, nonmem_model, nchains=4, threads_per_chain=8,
+                       n_runs) {
   has_md_folder <- check_folder(nonmem_model)
   
   # nonmem_dir = if sd nonmem_model/NONMEM/nonmem_model/chains/
@@ -13,18 +14,20 @@ run_chains <- function(folder, nonmem_model, nchains=4, threads_per_chain=8) {
   } else {
     nonmem_dir <- paste0(folder, "/NONMEM/", nonmem_model, "/chains/", nonmem_model)
   }
-  
-  future_map(
-    1:nchains,
-    ~ system(
+
+  for(run in 1:n_runs){
+    future_map(
+      1:nchains,
+      ~ system(
         paste0("execute ",
-               paste0(nonmem_dir, "-", .x, ".mod"),
+               paste0(nonmem_dir, "-run", run, "-", .x, ".mod"),
                " -parafile=/opt/NONMEM/nm751/run/mpilinux8.pnm",
                " -nodes=",
                threads_per_chain),
         wait = FALSE
-        )
-  )
+      )
+    )
+  }
 }
 
 check_folder <- function(filepath) {
@@ -33,3 +36,5 @@ check_folder <- function(filepath) {
   
   return(has_md_folder)
 }
+
+
